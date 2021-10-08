674a46cad381adc2cfafdfbb5a63cab6c6282bda
Sophia Müller-Dott <sophia.md@hotmail.de>
2021-10-08 18:42:23 +0200

    structured network construction and testing into collections

--- code/construct_networks.R
+++ code/construct_networks.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 source("code/constructNetwork_function.R")
 
+#### NTNU v1.0 ####
 # Construct different networks from curated NTNU information
 # load curated information
 GRNcuration <- read.csv('data/homogenized_resource.csv') %>%
@@ -43,7 +44,7 @@ saveRDS(GRN_signed_weighted_discrepancy_sign, "data/GRN_signed_weighted_discrepa
 GRN_signed_weighted_discrepancy_sign <- constructNetwork(GRNcuration = GRNcuration, curated_counts = 'scaled', mor = TRUE, mor_filter = 0.8, weight = "discrepancy_sign") %>% as_tibble()
 saveRDS(GRN_signed_weighted_discrepancy_sign, "data/GRN_signed_weighted_discrepancy_sign_0.8.rds")
 
-
+#### Dorothea ####
 # Construct different networks from dorothea
 # load curated information
 dorothea <- readRDS("data/dorothea_filtered.rds")
@@ -68,3 +69,93 @@ saveRDS(dorothea_agnostic_weighted, "data/dorothea_agnostic_weighted.rds")
 
 dorothea_signed_weighted <- merged_network %>% select(c(source, target, confidence.x, mor.x, likelihood.y)) %>% rename(confidence = confidence.x, likelihood = likelihood.y, mor = mor.x)
 saveRDS(dorothea_signed_weighted, "data/dorothea_signed_weighted.rds")
+
+#### NTNU v2.0, ExTRI ####
+# Construct different networks for ExTri data
+# First we load the GRN curation tables. GRNcuration_ExTRI_tot just contains the information for the total number of evidence but not the information for each resource separately. Here the duplicated pubmed IDs between resources are just count once.
+# The GRNcuration_ExTRI contains the information per resource in addition to the sum. If different ressources take their information from the same pubmed ID this is counted multiple times. The advantage is that we can scale the information for each resource.
+GRNcuration_ExTRI_tot <- read.table('data/evidence_counts_ExTRI_pairs.tsv', sep = "\t", header=FALSE) %>%
+  rename(TF.TG = V1, totPositive = V2, totNeg = V3, totUnknown = V4) %>%
+  column_to_rownames(var = "TF.TG")
+
+GRNcuration_ExTRI <- read.csv('data/homogenized_ressource_ExTRI.csv') %>%
+  column_to_rownames(var = "TF.TG")
+
+# To test the effect of ExTRI that information is removed from the table.
+GRNcuration_new <- GRNcuration_ExTRI %>%
+  mutate(totNeg = totNeg-ExTRI_Negative) %>%
+  mutate(totPositive = totPositive-ExTRI_Positive) %>%
+  mutate(totUnknown = totUnknown-ExTRI_Unknown) %>%
+  select(-c(ExTRI_Negative, ExTRI_Positive, ExTRI_Unknown)) %>%
+  filter(!totUnknown + totPositive + totNeg == 0)
+
+# The networks to construct are then collected in a data.frame. I used different data.frames to generate different collections I would like to test.
+networks_weights <- tibble(GRNcuration = list(GRNcuration_ExTRI),
+                          curated_counts = "scaled",
+                          mor = TRUE,
+                          mor_filter = NA,
+                          weight = c("none", "evidence", "evidence_sign", "discrepancy_sign"),
+                          rm_bidirectional_rows = TRUE)
+
+networks_sign <- tibble(GRNcuration = list(GRNcuration_ExTRI),
+                           curated_counts = "scaled",
+                           mor = TRUE,
+                           mor_filter = 0:10/10,
+                           weight = "evidence",
+                           rm_bidirectional_rows = TRUE)
+
+networks_tot <- tibble(GRNcuration = list(GRNcuration_ExTRI_tot),
+                           curated_counts = "standard",
+                           mor = c(FALSE, TRUE, FALSE, TRUE),
+                           mor_filter = NA,
+                           weight = c("none", "none", "evidence_tot", "evidence_tot"),
+                           rm_bidirectional_rows = TRUE)
+
+networks_comparison <- tibble(GRNcuration = list(GRNcuration_ExTRI),
+                             curated_counts = "scaled",
+                             mor = c(FALSE, TRUE, FALSE, TRUE),
+                             mor_filter = NA,
+                             weight = c("none", "none", "evidence", "evidence"),
+                             rm_bidirectional_rows = TRUE)
+
+networks_comparison_unrestricted <- networks_comparison %>% mutate(rm_bidirectional_rows = FALSE)
+
+network_collection <- list(ExTRI_weights = networks_weights,
+                           ExTRI_sign = networks_sign,
+                           ExTRI_tot_comp = networks_tot,
+                           ExTRI_comp = networks_comparison,
+                           ExTRI_comp_unrestricted = networks_comparison_unrestricted)
+
+for (network in names(network_collection)){
+    network_collection[[network]] <- network_collection[[network]] %>% add_column(path = paste0("data/", paste(network, network_collection[[network]]$curated_counts,
+                                                                                                               network_collection[[network]]$mor, network_collection[[network]]$mor_filter,
+                                                                                                               network_collection[[network]]$weight, network_collection[[network]]$rm_bidirectional_rows, sep = "_"), ".rds"))
+}
+
+saveRDS(network_collection, "data/network_collection.rds")
+
+# construct networks from the data.frames. the network within the constructNetwork function needs to be changed manually at the moment.
+network_size <- map(names(network_collection), function(network){
+  collection <- network_collection[[network]]
+  res <- pmap(collection, function(GRNcuration, curated_counts, mor, mor_filter, weight, rm_bidirectional_rows, path){
+    GRN <- constructNetwork(GRNcuration = GRNcuration,
+                            curated_counts = curated_counts,
+                            mor = mor, mor_filter = mor_filter,
+                            weight = weight,
+                            rm_bidirectional_rows = rm_bidirectional_rows) %>% as_tibble()
+    saveRDS(GRN, path)
+    network_size <- c(network = path,
+                      edges = length(GRN$source),
+                      TF = length(unique(GRN$source)))
+    return(network_size)
+})
+  res <- as.data.frame(do.call(rbind, res))
+  return(res)
+})
+
+network_size <- as.data.frame(do.call(rbind, network_size))
+network_size <- network_size %>% mutate(network = str_remove(str_remove(network_size$network, ".rds"), "data/")) %>%
+  add_row(network = "Dorothea_A", edges = "5378", TF = "94")
+
+write_csv(network_size, "data/network_size.csv")
+
--- code/test_networks.R
+++ code/test_networks.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(purrr)
 library(tidyverse)
 
+#### NTNU networks v1.0 ####
 # Paths to benchmark data, benchmark metadata and kinase substrate network
 # Benchmark data contains 82 perturbation experiments covering 27 unique kinases
 # Network contains 92 kinases with regulon size of 57 ± 86
@@ -123,3 +124,251 @@ ggplot(boxplot_tibble,aes(fill = network, x = statistic, y = auc)) +
 ggplot(boxplot_tibble,aes(fill = network, x = statistic, y = prc)) +
   geom_boxplot()
 
+
+
+#### NTNU networks v2.0 ####
+
+# Paths to benchmark data, benchmark metadata and kinase substrate network
+# Benchmark data contains 82 perturbation experiments covering 27 unique kinases
+# Network contains 92 kinases with regulon size of 57 ± 86
+bexample_url <- file.path('data', "dorothea_bench_expr.rds")
+bmeta_url <- file.path('data', "dorothea_bench_meta.rds")
+source_url <- file.path('data', "dorothea_filtered.rds")
+
+# Design contains statistical methods that take weights into account
+design_row <-
+  tibble(
+    set_name = "Dorothea_A", # name of the set resource
+    bench_name = "dbd", # name of the benchmark data
+    stats_list = list( # a list of the stats to call
+      c(
+        "scira",
+        "pscira",
+        "mean",
+        "viper"
+      )
+    ),
+    opts_list = list(list( # list of options for each stat method
+      scira = list(.mor = "mor",
+                   .likelihood = "likelihood"),
+      pscira = list(times = 100,
+                    .mor = "mor",
+                    .likelihood = "likelihood"),
+      mean = list(times = 100,
+                  .mor = "mor",
+                  .likelihood = "likelihood"),
+      viper = list(verbose = FALSE,
+                   minsize = 0,
+                   .mor = "mor",
+                   .likelihood = "likelihood",
+                   pleiotropy = TRUE,
+                   eset.filter = FALSE)
+    )),
+    bexpr_loc = bexample_url, # benchmark data location
+    bmeta_loc = bmeta_url, # metadata location
+    source_loc = source_url, # set source location
+    source_col = "source", # source name of the gene set source
+    target_col = "target", # target name of the set source'
+    filter_col = "confidence", # column by which we wish to filter
+    filter_crit = list(c("A")) # criteria by which we wish to filter
+  )
+
+network_collection <- readRDS("data/network_collection.rds")
+
+map(names(network_collection), function(name){
+  networks <- network_collection[[name]]
+  input_tibble <- design_row
+  # input tibble for one run with the weighted network
+  # and one where the weight is removed
+  for (i in 1:nrow(networks)){
+    input_tibble <- input_tibble %>% add_row(design_row %>%
+                                               mutate(set_name = str_remove(str_remove(networks$path[i], ".rds"), "data/")) %>%
+                                               mutate(source_loc = networks$path[i]))
+  }
+
+  # run decoupleRBenchmark
+  estimate <- run_benchmark(
+    .design = input_tibble, # provide input tibble
+    .minsize = 5, # filter gene sets with size < 5
+    .form = TRUE, # format the benchmark results
+    .perform = TRUE, # evaluate benchmarking performance
+    .silent = TRUE, # silently run the pipeline
+    .downsample_pr = TRUE, # downsample TNs for precision-recall curve
+    .downsample_roc = TRUE, # downsample TNs for ROC
+    .downsample_times = 100, # downsampling iterations
+    .url_bool = FALSE # whether to load from url
+  )
+
+
+  # extract each auc per permutation run
+  auc_downsampling <- lapply(estimate@bench_res$roc,
+                             function(x) x %>% group_by(run) %>% summarize(raw_auc = unique(raw_auc)) %>% pull(raw_auc))
+
+  estimate@bench_res <- add_column(estimate@bench_res, auc_downsampling)
+
+  prc_downsampling <- lapply(estimate@bench_res$prc,
+                             function(x) x %>% group_by(run) %>% summarize(raw_auc = unique(raw_auc)) %>% pull(raw_auc))
+
+  estimate@bench_res <- add_column(estimate@bench_res, prc_downsampling)
+
+  # boxplot sorted by methods with lowest p-value (t-test)
+  boxplot_tibble <- bind_cols(auc = unlist(auc_downsampling),
+                              prc = unlist(prc_downsampling),
+                              statistic = rep(estimate@bench_res$statistic, each = 100),
+                              network = rep(estimate@bench_res$set_name, each = 100))
+
+  boxplot_tibble$network <- factor(boxplot_tibble$network, levels = boxplot_tibble %>%
+                                     group_by(network) %>%
+                                     summarise(mean = mean(auc)) %>% arrange(mean) %>% pull(network))
+
+
+  auc <- ggplot(boxplot_tibble,aes(fill = network, x = statistic, y = auc)) +
+    geom_boxplot()
+
+  roc <- ggplot(boxplot_tibble,aes(fill = network, x = statistic, y = prc)) +
+    geom_boxplot()
+
+  ggsave(paste0("figures/auc/", name, ".png"), plot = auc, width = 1750, height = 800, units = "px")
+  ggsave(paste0("figures/roc/", name, ".png"), plot = roc, width = 1750, height = 800, units = "px")
+
+})
+
+
+#### old ####
+networks <- network_collection[["final_comp"]]
+
+input_tibble <- design_row
+# input tibble for one run with the weighted network
+# and one where the weight is removed
+for (i in 1:nrow(networks)){
+  input_tibble <- input_tibble %>% add_row(design_row %>%
+                        mutate(set_name =paste(networks$GRNcuration[i], networks$curated_counts[i], networks$mor[i], networks$weight[i], sep = "_")) %>%
+                        mutate(source_loc = file.path('data', paste0(networks$GRNcuration[i], "_",  networks$curated_counts[i], "_", networks$mor[i], "_", networks$weight[i], ".rds"))))
+}
+
+input_tibble <- input_tibble %>% add_row(design_row %>%
+                                           mutate(set_name = "GRNcuration_ExTRI_scaled_FALSE_evidence_unfiltered") %>%
+                                           mutate(source_loc = file.path('data',"GRNcuration_ExTRI_scaled_FALSE_scaledunfiltered.rds"))) %>%
+  add_row(design_row %>%
+            mutate(set_name = "GRNcuration_ExTRI_scaled_FALSE_none_unfiltered") %>%
+            mutate(source_loc = file.path('data',"GRNcuration_ExTRI_scaled_FALSE_none_unfiltered.rds")))
+
+
+# run decoupleRBenchmark
+estimate <- run_benchmark(
+  .design = input_tibble, # provide input tibble
+  .minsize = 5, # filter gene sets with size < 5
+  .form = TRUE, # format the benchmark results
+  .perform = TRUE, # evaluate benchmarking performance
+  .silent = TRUE, # silently run the pipeline
+  .downsample_pr = TRUE, # downsample TNs for precision-recall curve
+  .downsample_roc = TRUE, # downsample TNs for ROC
+  .downsample_times = 100, # downsampling iterations
+  .url_bool = FALSE # whether to load from url
+)
+
+
+# extract each auc per permutation run
+auc_downsampling <- lapply(estimate@bench_res$roc,
+                           function(x) x %>% group_by(run) %>% summarize(raw_auc = unique(raw_auc)) %>% pull(raw_auc))
+
+estimate@bench_res <- add_column(estimate@bench_res, auc_downsampling)
+
+prc_downsampling <- lapply(estimate@bench_res$prc,
+                           function(x) x %>% group_by(run) %>% summarize(raw_auc = unique(raw_auc)) %>% pull(raw_auc))
+
+estimate@bench_res <- add_column(estimate@bench_res, prc_downsampling)
+
+# boxplot sorted by methods with lowest p-value (t-test)
+boxplot_tibble <- bind_cols(auc = unlist(auc_downsampling),
+                            prc = unlist(prc_downsampling),
+                            statistic = rep(estimate@bench_res$statistic, each = 100),
+                            network = rep(estimate@bench_res$set_name, each = 100))
+
+boxplot_tibble <- boxplot_tibble %>% filter(!network == "GRNcuration_ExTRI_scaled_FALSE_none_unfiltered") %>% filter(!network == "GRNcuration_new_scaled_TRUE_evidence")
+
+boxplot_tibble <- boxplot_tibble %>% mutate(network = replace(network, network == "GRNcuration_ExTRI_scaled_FALSE_none" , "GRN_agnostic"),
+                                            network = replace(network, network == "GRNcuration_ExTRI_scaled_FALSE_evidence" , "GRN_weighted"),
+                                            network = replace(network, network == "GRNcuration_ExTRI_scaled_TRUE_evidence" , "GRN_signed_weighted"),
+                                            network = replace(network, network == "GRNcuration_ExTRI_tot_standard_TRUE_evidence_tot" , "GRN_total_signed_weighted"),
+                                            network = replace(network, network == "GRNcuration_ExTRI_scaled_FALSE_evidence_unfiltered" , "GRN_weighted_unfiltered"))
+
+
+boxplot_tibble$network <- factor(boxplot_tibble$network, levels = boxplot_tibble %>%
+                                   group_by(network) %>%
+                                   summarise(mean = mean(auc)) %>% arrange(mean) %>% pull(network))
+
+
+ggplot(boxplot_tibble,aes(fill = network, x = statistic, y = auc)) +
+  geom_boxplot()
+
+ggplot(boxplot_tibble,aes(fill = network, x = statistic, y = prc)) +
+  geom_boxplot()
+
+
+## Compare networks with same number of TFs
+input_tibble_size <- design_row %>% add_row(design_row %>%
+                                              mutate(set_name = "ExTRI_signed_weighted_resticted") %>%
+                                              mutate(source_loc = "data/ExTRI_signed_weighted_restricted.rds")) %>%
+  add_row(design_row %>%
+            mutate(set_name = "ExTRI_weighted_unfiltered_resticted") %>%
+            mutate(source_loc = "data/ExTRI_weighted_unfiltered_restricted.rds"))
+
+input_tibble_size <- design_row %>% add_row(design_row %>%
+                                              mutate(set_name = "ExTRI_signed_weighted") %>%
+                                              mutate(source_loc = "data/GRNcuration_ExTRI_scaled_TRUE_evidence.rds")) %>%
+  add_row(design_row %>%
+            mutate(set_name = "ExTRI_signed_weighted_unrestricted") %>%
+            mutate(source_loc = "data/GRN_tmp.rds"))
+
+
+# run decoupleRBenchmark
+estimate <- run_benchmark(
+  .design = input_tibble_size, # provide input tibble
+  .minsize = 5, # filter gene sets with size < 5
+  .form = TRUE, # format the benchmark results
+  .perform = TRUE, # evaluate benchmarking performance
+  .silent = TRUE, # silently run the pipeline
+  .downsample_pr = TRUE, # downsample TNs for precision-recall curve
+  .downsample_roc = TRUE, # downsample TNs for ROC
+  .downsample_times = 100, # downsampling iterations
+  .url_bool = FALSE # whether to load from url
+)
+
+
+# extract each auc per permutation run
+auc_downsampling <- lapply(estimate@bench_res$roc,
+                           function(x) x %>% group_by(run) %>% summarize(raw_auc = unique(raw_auc)) %>% pull(raw_auc))
+
+estimate@bench_res <- add_column(estimate@bench_res, auc_downsampling)
+
+prc_downsampling <- lapply(estimate@bench_res$prc,
+                           function(x) x %>% group_by(run) %>% summarize(raw_auc = unique(raw_auc)) %>% pull(raw_auc))
+
+estimate@bench_res <- add_column(estimate@bench_res, prc_downsampling)
+
+# boxplot sorted by methods with lowest p-value (t-test)
+boxplot_tibble <- bind_cols(auc = unlist(auc_downsampling),
+                            prc = unlist(prc_downsampling),
+                            statistic = rep(estimate@bench_res$statistic, each = 100),
+                            network = rep(estimate@bench_res$set_name, each = 100))
+
+boxplot_tibble <- boxplot_tibble %>% filter(!network == "GRNcuration_ExTRI_scaled_FALSE_none_unfiltered") %>% filter(!network == "GRNcuration_new_scaled_TRUE_evidence")
+
+boxplot_tibble <- boxplot_tibble %>% mutate(network = replace(network, network == "GRNcuration_ExTRI_scaled_FALSE_none" , "GRN_agnostic"),
+                                            network = replace(network, network == "GRNcuration_ExTRI_scaled_FALSE_evidence" , "GRN_weighted"),
+                                            network = replace(network, network == "GRNcuration_ExTRI_scaled_TRUE_evidence" , "GRN_signed_weighted"),
+                                            network = replace(network, network == "GRNcuration_ExTRI_tot_standard_TRUE_evidence_tot" , "GRN_total_signed_weighted"),
+                                            network = replace(network, network == "GRNcuration_ExTRI_scaled_FALSE_evidence_unfiltered" , "GRN_weighted_unfiltered"))
+
+
+boxplot_tibble$network <- factor(boxplot_tibble$network, levels = boxplot_tibble %>%
+                                   group_by(network) %>%
+                                   summarise(mean = mean(auc)) %>% arrange(mean) %>% pull(network))
+
+
+ggplot(boxplot_tibble,aes(fill = network, x = statistic, y = auc)) +
+  geom_boxplot()
+
+ggplot(boxplot_tibble,aes(fill = network, x = statistic, y = prc)) +
+  geom_boxplot()
