9a21e1149ec8d741cc105c995c2a3411aaf5e871
Sophia Müller-Dott <sophia.md@hotmail.de>
2023-01-20 13:33:55 +0100

    filtered network before running matrixRider

--- code/03.1_weights_matrixRider.R
+++ code/03.1_weights_matrixRider.R
@@ -21,7 +21,7 @@ GRN <- collecTRI_homogenized %>%
 
 
 # construct or load promoter sequence of target genes
-prom_length <- "1000"
+prom_length <- "10000"
 dir.create(file.path("data", "promoter_sequence"), showWarnings = FALSE)
 
 if (length(list.files(file.path("data", "promoter_sequence"),
@@ -39,35 +39,35 @@ symbols <- unique(GRN$target)
 ids <- mapIds(org.Hs.eg.db, symbols, 'ENTREZID', 'ALIAS')
 ids <- ids[!is.na(ids)]
 
+# Filter GRN for TFs and genes with available promoter sequence and binding motif
+GRN <- left_join(GRN, ids %>%
+            as.data.frame() %>%
+            rownames_to_column("target")) %>%
+  dplyr::rename("id" = ".")
+
+
+GRN_filtered <- GRN %>%
+  filter(source %in% names(pwm_tfs) & id %in% names(prom_sequence)) %>%
+  filter(!is.na(id))
+
+dim(GRN_filtered)
 
 ## calculate probabilities ---------------------------
-
-GRN_prob <- map_dfr(1:nrow(GRN), function(j){
+GRN_prob <- map_dfr(1:nrow(GRN_filtered), function(j){
   print(j)
-  x <- GRN[j,]
-  if (any(names(ids) == x$target)){
-    gene_id <- ids[names(ids) == x$target]
-  } else {
-    gene_id <- c("no gene id")
-  }
+  x <- GRN_filtered[j,]
 
-  if (x$source %in% names(pwm_tfs) & gene_id %in% names(prom_sequence)){
-    pfm <- pwm_tfs[[x$source]]
-    motif_length <- pfm@profileMatrix %>% ncol()
-    sequ <- prom_sequence[[gene_id]][[1]]
-    bind_aff <- log(MatrixRider::getSeqOccupancy(sequ, pfm, 0), base = 10)
+  pfm <- pwm_tfs[[x$source]]
+  motif_length <- pfm@profileMatrix %>% ncol()
+  sequ <- prom_sequence[[x$id]][[1]]
+  bind_aff <- log(MatrixRider::getSeqOccupancy(sequ, pfm, 0), base = 10)
 
-    data.frame(source = x$source,
-               target = x$target,
-               bind_aff = bind_aff,
-               motif_length = motif_length)
-  } else {
-    data.frame(source = x$source,
-               target = x$target,
-               bind_aff = NA,
-               motif_length = NA)
-  }
+  data.frame(source = x$source,
+             target = x$target,
+             bind_aff = bind_aff,
+             motif_length = motif_length)
+
 })
 
 ## save results ---------------------------
-saveRDS(GRN_prob, file.path("output", file.version, "03_weighted_strategies", paste0("matrixRider_res_", prom_length, "bp.rds")))
+saveRDS(GRN_prob, file.path("output", file.version, "03_weighting_strategies", paste0("matrixRider_res_", prom_length, "bp.rds")))
