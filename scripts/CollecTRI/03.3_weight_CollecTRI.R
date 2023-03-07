d7c537068cee137eebcfa9a1a7d4979d04c881fc
Sophia Müller-Dott <sophia.md@hotmail.de>
2023-03-07 16:50:41 +0100

    code clean-up

--- scripts/CollecTRI/03.3_weight_CollecTRI.R
+++ scripts/CollecTRI/03.3_weight_CollecTRI.R
@@ -1,46 +1,32 @@
-# Copyright (c) Sophia Müller-Dott [2022]
-# sophia.mueller-dott@uni-heidelberg.de
-
-#' In this script we will construct weighted networks from
-#' 1) matrixRider
-#' 2) FIMO
-#' 3) (RcisTarget)
-
+#' In this script we will assign the weights to CollecTRI and try different
+#' normalisation strategies. To run this script you need to first calculate
+#' weights using matrixRider and FIMO as descriped in 03.1_weights_matrixRider.R
+#' and 03.2_weights_FIMO.R.
 
 library(tidyverse)
+set.seed(713)
 
 ## Load data ---------------------------
-# define version and load signed network
-file.version <- "040722"
-collecTRI_homogenized <- read_csv(file.path("output", file.version, "02_signed_networks", "strict_signed_collecTRI.csv")) %>%
-  mutate(edge = paste(source, target, sep = "."))
+output.folder <- "output"
+collecTRI <- read_csv( "output/CollecTRI/CollecTRI.csv") %>%
+  dplyr::mutate(edge = paste(source, target, sep = "."))
 
-matrixRider.res.1 <-  readRDS(file.path("output", file.version, "03_weighting_strategies", "matrixRider_res_1000bp.rds")) %>%
-  mutate(edge = paste(source, target, sep = ".")) %>%
-  arrange(edge)
-matrixRider.res.10 <-  readRDS(file.path("output", file.version, "03_weighting_strategies", "matrixRider_res_10000bp.rds") )%>%
-  mutate(edge = paste(source, target, sep = ".")) %>%
-  arrange(edge)
-FIMO.res.1 <- readRDS(file.path("output", file.version, "03_weighting_strategies", "FIMO_res_1000bp.rds")) %>%
-  mutate(edge = paste(source, target, sep = ".")) %>%
-  arrange(edge)
-FIMO.res.1 <- FIMO.res.1[!duplicated(FIMO.res.1$edge),]
-FIMO.res.10 <- readRDS(file.path("output", file.version, "03_weighting_strategies", "FIMO_res_10000bp.rds")) %>%
-  mutate(edge = paste(source, target, sep = ".")) %>%
-  arrange(edge)
-FIMO.res.10 <- FIMO.res.10[!duplicated(FIMO.res.10$edge),]
+weigths.files <- list.files(file.path("output", "weighted_networks", "raw"), full.names = T)
 
-weights_networks <- list(matrixRider.res.1, matrixRider.res.10, FIMO.res.1, FIMO.res.10)
-names(weights_networks) <- c("matrixRider.res.1", "matrixRider.res.10", "FIMO.res.1", "FIMO.res.10")
+weights_networks <- map(weigths.files, read_csv)
+weights_networks <- map(weights_networks, function(net){
+  net %>%
+    dplyr::mutate(edge = paste(source, target, sep = ".")) %>%
+    dplyr::arrange(edge)
+})
+names(weights_networks) <- str_remove(list.files(file.path("output", "weighted_networks", "raw")), ".csv")
 
-dir.create(file.path("output", file.version, "04_weighted_networks"), showWarnings = FALSE)
-
-set.seed(123)
+dir.create(file.path(output.folder, "weighted_networks"), showWarnings = FALSE)
 
 ## weight networks ---------------------------
 norm_net <- map_dfc(names(weights_networks), function(idx_net){
   net <- weights_networks[[idx_net]]
-  net <- left_join(net, collecTRI_homogenized %>% select(edge, weight)) %>%
+  net <- left_join(net, collecTRI %>% select(edge, weight), by = "edge") %>%
     rename("mor" = weight)
 
   if(str_detect(idx_net, "matrixRider")){
@@ -50,29 +36,33 @@ norm_net <- map_dfc(names(weights_networks), function(idx_net){
     write_csv(net_filtered %>%
                 mutate(weight = bind_aff * mor) %>%
                 select(source, target, weight),
-              file.path("output", file.version, "04_weighted_networks", paste0(idx_net, ".raw.csv")))
+              file.path(output.folder,"weighted_networks", paste0(idx_net, "_raw.csv")))
 
     net_gene <- map_dfr(unique(net_filtered$target), function(tg){
       GRN_gene <- net_filtered %>% dplyr::filter(target == tg)
 
       GRN_gene %>% mutate(bind_aff_gene = (GRN_gene$bind_aff) / (max(GRN_gene$bind_aff, na.rm = T)))
     })
+    net_gene <- net_gene %>%
+      dplyr::arrange(edge)
 
     write_csv(net_gene %>%
                 mutate(weight = bind_aff_gene * mor) %>%
                 select(source, target, weight),
-              file.path("output", file.version, "04_weighted_networks", paste0(idx_net, ".geneNorm.csv")))
+              file.path(output.folder,"weighted_networks", paste0(idx_net, "_geneNorm.csv")))
 
     net_TF <- map_dfr(unique(net_filtered$source), function(tf){
       GRN_tf <- net_filtered %>% dplyr::filter(source == tf)
 
       GRN_tf %>% mutate(bind_aff_tf = (GRN_tf$bind_aff) / (max(GRN_tf$bind_aff, na.rm = T)))
     })
+    net_TF <- net_TF %>%
+      dplyr::arrange(edge)
 
     write_csv(net_TF %>%
                 mutate(weight = bind_aff_tf * mor) %>%
                 select(source, target, weight),
-              file.path("output", file.version, "04_weighted_networks", paste0(idx_net, ".tfNorm.csv")))
+              file.path(output.folder,"weighted_networks", paste0(idx_net, "_tfNorm.csv")))
 
     weights_df <- data.frame(edge = net_filtered$edge,
                raw = net_filtered$bind_aff *net_filtered$mor,
@@ -89,7 +79,7 @@ norm_net <- map_dfc(names(weights_networks), function(idx_net){
     write_csv(net_filtered %>%
                 mutate(weight = score * mor) %>%
                 select(source, target, weight),
-              file.path("output", file.version, "04_weighted_networks", paste0(idx_net, ".raw.csv")))
+              file.path(output.folder,"weighted_networks", paste0(idx_net,"_raw.csv")))
 
     net_gene <- map_dfr(unique(net_filtered$target), function(tg){
       GRN_gene <- net_filtered %>% dplyr::filter(target == tg)
@@ -97,10 +87,13 @@ norm_net <- map_dfc(names(weights_networks), function(idx_net){
       GRN_gene %>% mutate(bind_aff_gene = (GRN_gene$score) / (max(GRN_gene$score, na.rm = T)))
     })
 
+    net_gene <- net_gene %>%
+      dplyr::arrange(edge)
+
     write_csv(net_gene %>%
                 mutate(weight = bind_aff_gene * mor) %>%
                 select(source, target, weight),
-              file.path("output", file.version, "04_weighted_networks", paste0(idx_net, ".geneNorm.csv")))
+              file.path(output.folder,"weighted_networks", paste0(idx_net, "_geneNorm.csv")))
 
     net_TF <- map_dfr(unique(net_filtered$source), function(tf){
       GRN_tf <- net_filtered %>% dplyr::filter(source == tf)
@@ -108,10 +101,13 @@ norm_net <- map_dfc(names(weights_networks), function(idx_net){
       GRN_tf %>% mutate(bind_aff_tf = (GRN_tf$score) / (max(GRN_tf$score, na.rm = T)))
     })
 
+    net_TF <- net_TF %>%
+      dplyr::arrange(edge)
+
     write_csv(net_TF %>%
                 mutate(weight = bind_aff_tf * mor) %>%
                 select(source, target, weight),
-              file.path("output", file.version, "04_weighted_networks", paste0(idx_net, ".tfNorm.csv")))
+              file.path(output.folder,"weighted_networks", paste0(idx_net, "_tfNorm.csv")))
 
     weights_df <- data.frame(edge = net_filtered$edge,
                              raw = net_filtered$score *net_filtered$mor,
@@ -121,16 +117,21 @@ norm_net <- map_dfc(names(weights_networks), function(idx_net){
     weights_df
   }
 })
-rownames(norm_net) <- norm_net$matrixRider.res.1_edge
+
+
+## compare correlation of weights ---------------------------
+rownames(norm_net) <- norm_net$matrixRider_10000bp_edge
 norm_net <- norm_net[!str_detect(colnames(norm_net), "edge")]
-norm_net <- cbind(norm_net, unweighted = sign(norm_net$matrixRider.res.1_raw))
-cor(norm_net)
+norm_net <- cbind(norm_net, unweighted = sign(norm_net$matrixRider_1000bp_raw))
+
+write_csv(cor(norm_net) %>% as.data.frame(),
+          file.path(output.folder,"weighted_networks", "correlation_matrix.csv"))
+
 
 ## filter networks ---------------------------
 net_filtered <- map(names(weights_networks), function(idx_net){
-  print(idx_net)
   net <- weights_networks[[idx_net]]
-  net <- left_join(net, collecTRI_homogenized %>% select(edge, weight))
+  net <- left_join(net, collecTRI %>% select(edge, weight), by = "edge")
 
   if (str_detect(idx_net, "matrixRider")){
     net <- net %>%
@@ -145,19 +146,20 @@ net_filtered <- map(names(weights_networks), function(idx_net){
       slice(1:n_to_keep) %>%
       select(source, target, weight)
 
-    write_csv(net_filtered, file.path("output", file.version, "04_weighted_networks", "filtered", paste0(idx_net, ".filtered", q_n,".csv")))
+    write_csv(net_filtered, file.path(output.folder, "weighted_networks", paste0(idx_net, "_filtered", q_n,".csv")))
     net_filtered
   })
 })
 
-net_unweighted <- weights_networks$matrixRider.res.1 %>%
-  left_join(collecTRI_homogenized %>% select(edge, weight)) %>%
+net_unweighted <- weights_networks$matrixRider_1000bp %>%
+  left_join(collecTRI %>% select(edge, weight), by = "edge") %>%
   select(source, target, weight)
 
-net_random10 <- net_unweighted[sample(nrow(net_unweighted), nrow(net) - round(nrow(net)/100 * 10)), ]
-net_random20 <- net_unweighted[sample(nrow(net_unweighted), nrow(net) - round(nrow(net)/100 * 20)), ]
-net_random30 <- net_unweighted[sample(nrow(net_unweighted), nrow(net) - round(nrow(net)/100 * 30)), ]
+net_random10 <- net_unweighted[sample(nrow(net_unweighted), nrow(net_unweighted) - round(nrow(net_unweighted)/100 * 10)), ]
+net_random20 <- net_unweighted[sample(nrow(net_unweighted), nrow(net_unweighted) - round(nrow(net_unweighted)/100 * 20)), ]
+net_random30 <- net_unweighted[sample(nrow(net_unweighted), nrow(net_unweighted) - round(nrow(net_unweighted)/100 * 30)), ]
 
-write_csv(net_random10, file.path("output", file.version, "04_weighted_networks", "filtered", "random10.csv"))
-write_csv(net_random20, file.path("output", file.version, "04_weighted_networks", "filtered", "random20.csv"))
-write_csv(net_random30, file.path("output", file.version, "04_weighted_networks", "filtered", "random30.csv"))
+write_csv(net_random10, file.path(output.folder, "weighted_networks", "random10.csv"))
+write_csv(net_random20, file.path(output.folder, "weighted_networks", "random20.csv"))
+write_csv(net_random30, file.path(output.folder, "weighted_networks", "random30.csv"))
+
